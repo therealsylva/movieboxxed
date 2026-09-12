a2615a1bd9d627035a6b2214bb428f9d854445aa
Al-Sahir <sylvesgodswill@gmail.com>
2026-09-12 11:27:52 +0100

    Bump release to 0.3.10

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.therealsylva.roaches"
         minSdk = 26
         targetSdk = 35
-        versionCode = 22
-        versionName = "0.3.9"
+        versionCode = 23
+        versionName = "0.3.10"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
     }
